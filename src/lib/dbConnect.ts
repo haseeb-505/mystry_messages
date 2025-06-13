dd7b903d769c3ab12ec04a842a13bf0e1f3d8d3a
haseeb-505 <hatahir87626@gmail.com>
2025-06-13 16:47:39 +0500

    added logic to delete the unverified expired user as soon as db connection is established

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,3 +1,5 @@
+import UserModel from "@/models/User";
+import { darcula } from "@react-email/components";
 import mongoose from "mongoose";
 
 type ConnectionObject = {
@@ -16,6 +18,18 @@ async function dbConnect(): Promise<void> {
         const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
         connection.isConnected = db.connections[0].readyState;
         console.log("DB Connected Successfully!!")
+
+        // delete unverified user after code expires
+        try {
+            const deleted = await UserModel.deleteMany({
+                isVerified: false,
+                verifyCodeExpiry : { $lt: new Date() }
+            });
+            console.log(`Deleted ${deleted.deletedCount} unverified users with expired codes`)
+        } catch (cleanupError) {
+            console.error("Error cleaning unverified expired users: ", cleanupError);   
+        }
+
     } catch (error) {
         console.log("DB connection failed ", error)
         process.exit(1)
